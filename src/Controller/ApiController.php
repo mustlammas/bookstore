eba8c0e8792838de272c0066536518d72d32b5bc
Marko Kurm <marko.kurm@gmail.com>
2018-05-10 11:09:21 +0300

    Rename directory. Construct a Book in the controller.

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -5,12 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use \Datetime;
+use App\Dto\Book;
 
 class ApiController extends Controller
 {
     public function list(LoggerInterface $logger)
     {
          $logger->info('We are logging!');
+
+         $book = new Book("978-1-56619-909-4 ", "I Was Told There'd Be Cake", new DateTime());
+
          return new Response('<html><body>Hello, world!</body></html>');
     }
 }
