bce1c88750ef99f2ce390f6d07c2542139c9c990
Marko Kurm <marko.kurm@gmail.com>
2018-05-10 14:38:39 +0300

    Remove the controller test and use the ApiClientTest instead.

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -11,14 +11,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Psr\Log\LoggerInterface;
 
 class ApiController extends Controller
 {
     private $serializer;
     private $books;
+    private $logger;
 
-    public function __construct(JsonSerializer $serializer) {
+    public function __construct(JsonSerializer $serializer, LoggerInterface $logger) {
         $this->serializer = $serializer;
+        $this->logger = $logger;
         $this->books = new InMemoryBookStore();
 
         $this->books->add(new Book("978-1-56619-909-4", "I Was Told There'd Be Cake", new DateTime()));
@@ -37,13 +40,15 @@ class ApiController extends Controller
     }
 
     /**
-     * @Route("/api/v1/books")
+     * @Route("/api/v1/books/{isbn}")
      * @Method("POST")
      */
-    public function add($json)
+    public function add($isbn)
     {
-        $book = $this->serializer->deserialize($json, Book::class);
-        $this->books->add($book);
+        $this->logger->info("Adding book: " . $isbn);
+        //$book = $this->serializer->deserialize($json, Book::class);
+        //$this->books->add($book);
+        return new JsonResponse();
     }
 }
 
--- tests/ApiClientTest.php
+++ tests/ApiClientTest.php
@@ -11,14 +11,15 @@ class ApiControllerTest extends TestCase
     public function test()
     {
         $headers = array('Accept' => 'application/json');
+        $isbn = "978-1-56619-666-5";
         $data = array(
-            "isbn" => "978-1-56619-666-5",
+            "isbn" => $isbn,
             "title" => "I Was Told There'd Be Cake 2",
             "addedOn" => "2000-01-10T08:41:33+00:00"
         );
         $body = Body::json($data);
-        $response = Request::post('http://localhost:8000/api/v1/books', $headers, $body);
-        echo "\nHTTP Response code: " . $response->code;
+        $response = Request::post('http://localhost:8000/api/v1/books/' . $isbn, $headers, $body);
+        $this->assertEquals(200, $response->code);
     }
 }
 
