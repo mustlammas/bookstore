d204ba5b841d5b12bf02d5ca4b657a1299803f17
Marko Kurm <marko.kurm@gmail.com>
2018-05-10 11:16:19 +0300

    Require symfony/property-access. Serialize the Book object and return it.

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -8,15 +8,28 @@ use Symfony\Component\HttpFoundation\Response;
 use \Datetime;
 use App\Dto\Book;
 
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
 class ApiController extends Controller
 {
+    private $serializer;
+
+    public function __construct() {
+        $encoders = array(new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+        $this->serializer = new Serializer($normalizers, $encoders);
+    }
+
     public function list(LoggerInterface $logger)
     {
-         $logger->info('We are logging!');
-
          $book = new Book("978-1-56619-909-4 ", "I Was Told There'd Be Cake", new DateTime());
+         $json = $this->serializer->serialize($book, 'json');
+         # $logger->info($json);
 
-         return new Response('<html><body>Hello, world!</body></html>');
+         return new Response($json);
     }
 }
 
